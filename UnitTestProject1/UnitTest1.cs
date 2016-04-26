7ab013d9ee5809030b1415b583d1a5768a891c61
Jani Järvinen <jani.jarvinen@intertechno.org>
2016-04-26 10:49:35 +0300

    Lisätty sovellukseen yksikkötestiprojekti sekä yksinkertainen yksikkötesti päivämäärän käsittelyyn.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication3.Utilities;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PäivämääräTestit()
+        {
+            // kelvollinen päivämäärä
+            string syöte = "26.4.2016";
+            DateTime tulos = DateParsing.ParseFinnishDate(syöte);
+            DateTime odotettu = new DateTime(2016, 4, 26);
+            Assert.AreEqual(odotettu, tulos);
+
+            // karkauspäivämäärä
+            syöte = "29.2.2016";
+            tulos = DateParsing.ParseFinnishDate(syöte);
+            odotettu = new DateTime(2016, 2, 29);
+            Assert.AreEqual(odotettu, tulos);
+
+            // virheellinen syöte
+            syöte = "";
+            tulos = DateParsing.ParseFinnishDate(syöte);
+            odotettu = DateTime.MinValue;
+            Assert.AreEqual(odotettu, tulos);
+
+            // virheellinen syöte 2
+            syöte = null;
+            tulos = DateParsing.ParseFinnishDate(syöte);
+            odotettu = DateTime.MinValue;
+            Assert.AreEqual(odotettu, tulos);
+
+            // virheellinen päivämäärä
+            syöte = "4/26/2016";
+            tulos = DateParsing.ParseFinnishDate(syöte);
+            odotettu = DateTime.MinValue;
+            Assert.AreEqual(odotettu, tulos);
+        }
+    }
+}
---
+++ WebApplication3/Utilities/DateParsing.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Utilities
+{
+    public class DateParsing
+    {
+        public static DateTime ParseFinnishDate(string date)
+        {
+            DateTime result = DateTime.MinValue;
+            string finnishFormat = "d.M.yyyy";
+            try
+            {
+                CultureInfo fiFi = new CultureInfo("fi-FI");
+                result = DateTime.ParseExact(date, finnishFormat, fiFi);
+            }
+            catch (Exception ex)
+            {
+                // TODO: kirjoitetaan poikkeu lokiin
+                result = DateTime.MinValue;
+            }
+
+            // palautetaan tulos
+            return result;
+        }
+    }
+}
